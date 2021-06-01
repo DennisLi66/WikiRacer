3b8693cc4f0cf9a8eb634d3ddbb11ecb4398b644
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-05-31 19:08:44 -0700

    Basic WIkiRacer Express Server Started

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const cookieParser = require('cookie-parser');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const randomatic = require('randomatic');
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+// var connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+//   multipleStatements: true
+// })
+// connection.connect();
+
+//FIX THIS ADD DOTENV FILE
+//FIX THIS ADD PROPER MYSQL FOR HIGH SCORES
+
+app.listen(3000, function() {
+  console.log("Server Started.")
+});
