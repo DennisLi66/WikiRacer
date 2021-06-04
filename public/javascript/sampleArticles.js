3469fdcf4070c2af418cf8cf3d446972df1b757a
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-06-03 18:47:49 -0700

    Added a More Fair Difficulty Option

--- public/javascript/sampleArticles.js
+++ public/javascript/sampleArticles.js
@@ -1,7 +1,6 @@
 var articles = [
   "Abraham_Lincoln",
-  // "Derek Yu",
-  // "Jason Mraz",
+  "Jason Mraz",
   "Nintendo",
   "Jimi_Hendrix",
   "Cashew",
@@ -14,32 +13,55 @@ var articles = [
   "Marie Curie",
   "Napkin",
   "Raid:_Shadow_Legends",
-  // "M. Night Shyamalan",
-  // "Walt Disney",
+  "M. Night Shyamalan",
+  "Walt Disney",
   "Dentistry",
-  // "Aura_(paranormal)",
-  // "Artificial intelligence",
+  "Aura_(paranormal)",
+  "Artificial intelligence",
   "Emu",
   "Parkour",
-  // "Paradox",
+  "Paradox",
+  "Batman",
   "Neolithic Revolution"
 ]
-
 var goodStarts = [
-
+  "Napkin",
+  "Raid:_Shadow_Legends",
+  "M. Night Shyamalan",
+  "Walt Disney",
+  "Dentistry",
+  "Aura_(paranormal)",
+  "Artificial intelligence",
+  "Emu",
+  "Parkour",
+  "Paradox",
+  "Neolithic Revolution",
+  "Jason Mraz",
+  "Jimi_Hendrix",
+  "Javascript",
+  "Cashew",
+  "Count_Dracula",
+  "Javascript",
+  "Ikea",
+  "Nitrogen",
+  "Batman"
 ]
-
 var goodEnds = [
-
+  "Marie Curie",
+  "Abraham_Lincoln",
+  "Nintendo",
+  'Georgia_(Country)',
+  "Tornado",
+  "Halloween"
 ]
 
-function getTwoArticles(){
+function getTwoArticlesChaos() {
   var start2End = {};
   var start = -1;
   var end = -1;
   start = Math.floor(Math.random() * articles.length);
   end = Math.floor(Math.random() * articles.length);
-  while (start == end){
+  while (start == end) {
     end = Math.floor(Math.random() * articles.length);
   }
   start2End["start"] = articles[start];
@@ -48,4 +70,16 @@ function getTwoArticles(){
   //get those values out of articles and map them to this function
   console.log(start2End);
   return start2End;
+}
+function getTwoArticlesFair(){
+  var start2End = {};
+  start = Math.floor(Math.random() * goodStarts.length);
+  end = Math.floor(Math.random() * goodEnds.length);
+  start2End["start"] = goodStarts[start];
+  start2End["end"] = goodEnds[end];
+  console.log(start2End);
+  return start2End;
+}
+function getTwoTrueRandom(){
+  //ADD LATER
 }
