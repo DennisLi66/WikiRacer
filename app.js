ce496b8145959059fa02843eeee329a66459b436
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-06-06 22:49:13 -0700

    Checking that the page exists; fresh start

--- app.js
+++ app.js
@@ -52,10 +52,8 @@ app.post("/wikiracer",function(req,res){
 })
 
 app.get("/checkit",function(req,res){
-  //read to see if start and ends are valid using wikijs
-  //may need to replace .replace(/ /g,"_")
   var random = req.query.random;
-  console.log(random);
+  // console.log(random);
   if (req.query.random === 'true'){
     console.log("Accessing WikiJS for randomization...");
     wiki().random(2).then(results => {
@@ -69,41 +67,67 @@ app.get("/checkit",function(req,res){
   }
   else{
     console.log("Accessing WikiJS for checking...")
+    var isError = false;
     var start = req.query.start;
     var end = req.query.end;
-    console.log(start);
-    console.log(end);
-    wiki().page(start).then(page => {
-      console.log("The start was valid.");
-      wiki().page(end).then(page1 => {
-        console.log("The end was valid.");
-        console.log(page1)
-        //FIX THIS: Redirect off a disambiguation page, possibly use the first hyperlink
-        //FIX THIS, Proper Redirecting
-      })
-      .catch(function(error){
-          console.log("Failure");
-          res.render("wikiracer",{
-            banner:"WikiRacer",
-            errorMsg: ''
-          })
-      })
-    })
-    .catch(function(error){
-        console.log("Failure");
+    var sDisambiguation = false;
+    var eDisambiguation = false;
+    //1: Access Start Page
+    wiki().page(start).then(
+      page => {
+        console.log("Start Worked, so testing end");
+        wiki().page(end).then(
+          page => {
+            console.log("End Worked")
+          }, error => {
+            console.log("End Failed");
+            res.render("wikiracer",{
+                      banner:"WikiRacer",
+                      errorMsg: ''
+                    });
+          }
+        )
+      },error => {
+        console.log('Start Failed');
         res.render("wikiracer",{
-          banner:"WikiRacer",
-          errorMsg: ''
-        })
-    })
-  }
-  //if bad send to error page on render
-  //res.send("http://localhost:3000/description")
+                  banner:"WikiRacer",
+                  errorMsg: ''
+                });
+  })
+}
+    // .finally(function(){
+    //   if (!isError){
+    //     wiki().page(end).then(function(){
+    //       console.log("End Worked")
+    //     },function(error){
+    //       console.log("End Failed");
+    //       isError = true;
+    //     })
+    //     // .then(function(){
+    //     //   if (!isError && (sDisambiguation || eDisambiguation)){
+    //     //     console.log("Still Ambiguous");
+    //     //   }
+    //     //   else if (!isError && !sDisambiguation && !eDisambiguation){
+    //     //     console.log("Not Ambiguous");
+    //     //   }
+    //     // })
+    //   }
+    // })
+
+
 })
 app.get("/wikiracer/game",function(req,res){
   console.log(req.query.start);
   console.log(req.query.end);
 })
+
+app.route("wikiracer/start")
+  .get(function(req,res){
+    // if COOKIES
+    res.render("wikiRacerStart",{
+      banner: 'WikiRacer: Game'
+    })
+  })
 
 app.listen(3000, function() {
   console.log("Server Started.")
